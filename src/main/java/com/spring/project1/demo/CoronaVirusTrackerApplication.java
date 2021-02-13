872309e777ea9f0bb554fa4f7081ac326ad72c88
FaramarzAf <faramarzafzali@gmail.com>
2021-02-13 15:32:42 +0330

    docs added

--- src/main/java/com/spring/project1/demo/CoronaVirusTrackerApplication.java
+++ src/main/java/com/spring/project1/demo/CoronaVirusTrackerApplication.java
@@ -11,20 +11,5 @@ public class CoronaVirusTrackerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CoronaVirusTrackerApplication.class, args);
     }
-//https://www.creative-tim.com/
-    //https://www.codeply.com/p/sIIXzlAW3V
 
-/**
- * api covid
- * https://covid19.mathdro.id/api/confirmed
- */
-    /**
-     * chart links
-     * http://jsfiddle.net/gh/get/jquery/1.9.1/highslide-software/highcharts.com/tree/master/samples/highcharts/demo/column-basic/
-     *https://www.w3schools.com/howto/howto_google_charts.asp
-     *http://www.prepbootstrap.com/bootstrap-template/bootstrap-chart
-     * https://medium.com/wdstack/bootstrap-4-chart-js-39006427f08f
-     * https://www.codeply.com/go/3l6UhaQEhq/bootstrap-4-chartjs
-     * https://www.creative-tim.com/learning-lab/bootstrap/charts/argon-dashboard
-     */
 }
